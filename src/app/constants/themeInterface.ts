57f415f87fd2b87cd2ab80e6a17368082e073e44
huytv04883 <huytv04883@gmail.com>
2023-10-04 00:58:08 +0700

    API for posts

--- src/app/constants/themeInterface.ts
+++ src/app/constants/themeInterface.ts
@@ -8,3 +8,14 @@ export interface CurrentContextType {
 export interface ChildrenProps {
     children: ReactNode;
 }
+
+export interface PostProps {
+    id: string;
+    createdAt: string;
+    slug: string;
+    title: string;
+    desc: string;
+    img?: string;
+    views: number;
+    catSlug: string;
+}
