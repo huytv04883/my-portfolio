e747b463ab08ea3bd1ce48d05e33b2dab6cf9c66
huytv04883 <huytv04883@gmail.com>
2023-10-21 00:49:48 +0700

    Revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import Categories from "./components/categories/Categories";
 import Sidebar from "./components/sidebar/Sidebar";
 import { IPageProps } from "./constants/Theme.type";
 
-export default function Home() {
-  // const page = parseInt(searchParams.page) || 1;
-  // const { cat } = searchParams;
+export default function Home({ searchParams }: IPageProps) {
+  const page = parseInt(searchParams.page) || 1;
+  const { cat } = searchParams;
 
   return (
     <main>
       <div className="grid grid-cols-1fr-30 gap-8">
-        {/* <Sidebar /> */}
-        {/* <div className="flex flex-col">
+        <Sidebar />
+        <div className="flex flex-col">
           <Categories />
           <CardList page={page} cat={cat} />
-        </div> */}
+        </div>
       </div>
     </main>
   );
